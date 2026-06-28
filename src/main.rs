e14bff0b215545e4e2c719418591693a2f1db8ae
Quentin Harscoët <harscoet.quentin@gmail.com>
2026-06-28 22:28:11 +0200

    First commit, can export to simple html, lot of tags not yet handled

---
+++ src/main.rs
@@ -0,0 +1,386 @@
+use std::{fmt, fs::File, io::{BufRead, BufReader, Write}, path::Path};
+use regex::Regex;
+use std::sync::LazyLock;
+use itertools::Itertools;
+
+const BANK_COUNT : usize = 10;
+const FILENAMES : [&str;BANK_COUNT] = [
+    "zel_00",
+    "zel_01",
+    "zel_02",
+    "zel_03",
+    "zel_04",
+    "zel_05",
+    "zel_06",
+    "zel_07",
+    "zel_08",
+    "zel_99",
+];
+
+const LANGUAGES_COUNT : usize = 3;
+
+const LANGUAGES : [&str;LANGUAGES_COUNT] = [
+    "jp",
+    "us",
+    "fr",
+    // "sp",
+    // "de",
+    // "it"
+];
+
+const LANGUAGES_FULL : [&str;LANGUAGES_COUNT] = [
+    "Japanese",
+    "US English",
+    "French",
+    // "Spanish",
+    // "German",
+    // "Italian"
+];
+
+static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(?P<ID>[[:xdigit:]]+) (@(?P<slot>[[:xdigit:]]{4}) )?(?P<attribs>\[.+\]) = (?P<str>.+)?").unwrap());
+static RE_TAG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\\[x|z]\{(.*?)\}").unwrap());
+
+const COLORS_RGB : [&str; 9] = [
+    "#FFFFFF",
+    "#f07878",
+    "#aadc8c",
+    "#a0b4dc",
+    "#dcdc82",
+    "#b4c8e6",
+    "#c8a0dc",
+    "#ffffff",
+    "#dcaa78",
+];
+
+fn unpack_u16(v:u16) -> (u8,u8) {
+    (((v & 0xFF00) >> 8) as u8, (v & 0x00FF) as u8)
+}
+
+fn get_u16_from_payload(payload : &[u8], idx : usize) -> u16 {
+
+    let v1 = payload[idx];
+    let v2 = payload[idx + 1];
+
+    ((v1 as u16) << 8 | v2 as u16) as u16 
+}
+
+#[derive(Debug, Default)]
+struct Tag {
+    group : u8,
+    number : u16,
+    payload : Vec<u8>
+}
+
+impl fmt::Display for Tag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"{:0>2X}/{:0>4X} payload : {:X?}", self.group, self.number, self.payload)
+    }
+}
+
+impl std::str::FromStr for Tag {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        
+        if let Some(caps) = RE_TAG.captures(s) {
+            let args = caps.get(1).map_or("", |m| m.as_str());
+            let values = args.split(",").collect::<Vec<_>>();
+
+            let x_escape = s.starts_with(r"\x");
+            let start_idx = x_escape as usize;
+
+            if x_escape {
+                let (_total_size, group) = unpack_u16(u16::from_str_radix(values[start_idx], 16).unwrap_or_default());
+    
+                let number = u16::from_str_radix(values[start_idx + 1], 16).unwrap();
+                let payload : Vec<_> = values[start_idx+2..].iter().flat_map(|s| u16::from_str_radix(s, 16)).map(|v| unpack_u16(v)).flat_map(|(v1,v2)| [v1,v2]).collect();
+                // println!("{:#x}, {:#x}, {:?}", total_size, group, payload);
+
+                Ok(Tag { group:group, number:number, payload:payload})
+            } else {
+                Err("Z escapes not implemented yet")
+            }
+        } else  {
+            Err("Couldn't capture data")
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Message {
+    text : [String; LANGUAGES_COUNT],
+    attribs : String,
+    id : usize
+}
+
+#[derive(Debug)]
+enum TextPart<'a> {
+    Text(&'a str),
+    Tag(Tag)
+}
+
+impl Message {
+    fn is_empty(&self) -> bool {
+        self.text.iter().all(|str| str.is_empty())
+    }
+
+    fn get_html_formatted(&self, lang_id : usize, ignore_tags : bool) -> String {
+        
+        let raw = &self.text[lang_id];
+        let s = raw.replace(r"\n", "<br>");
+
+        if ignore_tags {
+            RE_TAG.replace_all(&s, "").to_string()
+        } else {
+            let tags_it = RE_TAG.find_iter(&self.text[lang_id]).flat_map(|m| m.as_str().parse::<Tag>());
+            let str_it = RE_TAG.split(&s);
+
+            let tags_it = RE_TAG.find_iter(&self.text[lang_id]).flat_map(|m| m.as_str().parse::<Tag>()).map(|t| TextPart::Tag(t));
+            let str_it = RE_TAG.split(&s).map(|s| TextPart::Text(s));
+
+
+            let mut res_str = String::new();
+
+            let mut current_color = 0;
+            let mut current_size = 100;
+            // for (s, tag) in str_it.zip(tags_it) {
+            for part in str_it.interleave(tags_it) {
+                match part {
+                    TextPart::Text(text) => res_str += text,
+                    TextPart::Tag(tag) => {
+                        if tag.group == 0xFF {
+                            if tag.number == 0 { // change color
+                                let new_color = tag.payload[0] as usize;
+                                if current_color != 0 {
+                                    res_str += "</span>";
+                                }
+                                if new_color != 0 {
+                                    res_str += &format!("<span style='color:{};'>", COLORS_RGB[new_color]);
+                                }
+                                current_color = new_color;
+                            } else if tag.number == 1 {
+                                println!("{}", tag);
+                                let new_size = get_u16_from_payload(&tag.payload, 0);
+                                
+                                println!("curr_size : {}, new_size : {}", current_size, new_size);
+                                if current_size != 100 {
+                                    res_str += "</span>"
+                                }
+                                if new_size != 100 {
+                                    res_str += &format!("<span style='font-size:{}%;'>", new_size);
+                                }
+        
+                                current_size = new_size;
+                            }
+                        }
+                    }
+                }                
+            }
+
+            res_str
+        }
+        
+    }
+
+    fn print_tags(&self, lang_id : usize) {        
+        let tags_it = RE_TAG.find_iter(&self.text[lang_id]).flat_map(|m| m.as_str().parse::<Tag>()).map(|t| TextPart::Tag(t));
+        let str_it = RE_TAG.split(&self.text[lang_id]).map(|s| TextPart::Text(s));
+        
+        for part in str_it.interleave(tags_it) {
+            println!("{:?}", part);
+        }
+    }
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"{:?}", self.text)
+    }
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Message { text: Default::default(), attribs: String::new(), id : 0}
+    }
+}
+
+
+#[derive(Default, Debug)]
+struct BMGParser {
+    msgs : [Vec<Message>; BANK_COUNT],
+}
+
+impl BMGParser {
+    
+    fn print(self) {
+        for (bank_id,msg_bank) in self.msgs.iter().enumerate() {
+            for (idx, msg) in msg_bank.iter().filter(|msg| !msg.is_empty()).enumerate() {
+                println!("{} : {:#x} : {}", bank_id, idx, msg);
+            }
+        }
+    }
+}
+
+trait Exporter {
+    fn new(filepath: &Path) -> Self;
+    fn begin(&mut self);
+    fn set_headers(&mut self);
+    fn add_row(&mut self , msg : &Message, ignore_tags : bool);
+    fn end(&mut self);
+}
+
+struct HTMLExporter {
+    file: Option<File>
+}
+
+impl Exporter for HTMLExporter  {
+    fn new(filepath: &Path) -> Self {
+        if let Ok(f) = File::create(filepath) {
+            HTMLExporter { file: Some(f) }
+        } else {
+            HTMLExporter {file: None}
+        }
+    }
+    
+    fn begin(&mut self) {
+        if let Some(f) = &mut self.file {
+            let _ = f.write(b"<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<table>");
+        }
+        
+    }
+
+    fn set_headers(&mut self) {
+        if let Some(f) = &mut self.file {
+            let mut s = "<thead>
+    <tr>".to_string();
+
+          for lang in LANGUAGES_FULL {
+              s +=  &format!("<th>{}</th>", lang);
+          }
+
+      s += "
+    </tr>
+  </thead>
+  <tbody>";
+
+          let _ = f.write(s.as_bytes());
+        }
+    }
+
+    fn add_row(&mut self, msg : &Message, ignore_tags : bool) {
+        if let Some(f) = &mut self.file {
+            let mut s  = "<tr>".to_string();
+    
+            for i in 0..LANGUAGES_COUNT {
+                s += &format!("<td>{}</td>\n", msg.get_html_formatted(i, ignore_tags));
+            }
+    
+            s += "</tr>";
+    
+            let _ = f.write(s.as_bytes());
+        }
+    }
+
+    fn end(&mut self) {
+        if let Some(f) = &mut self.file {
+            let _ = f.write(b"</tbody>
+</table>
+</body>
+</html>");
+        }
+    }
+    
+}
+
+impl BMGParser {
+    fn feed_line(&mut self, line: &str, lang_idx : usize, bank_id : usize) { 
+        //println!("{}", line);
+        if let Some(groups) = RE.captures(line) {
+            let id: usize = usize::from_str_radix(&groups["ID"], 16).unwrap_or_default();
+
+            if id > 0 {
+                //let slot = usize::from_str_radix(&groups["slot"], 16).unwrap();
+                let idx = id -1;
+                if idx + 1> self.msgs[bank_id].len() { self.msgs[bank_id].resize_with(idx + 1, || Message::default() );}
+           
+                self.msgs[bank_id][idx].id = id;
+
+                if self.msgs[bank_id][idx].attribs.is_empty() {
+                    let attribs = &groups["attribs"];
+                    self.msgs[bank_id][idx].attribs = attribs.to_string();
+                }
+
+                if let Some(str) = groups.name("str")
+                {
+                    if !self.msgs[bank_id][idx].text[lang_idx].is_empty()
+                    {
+                        println!("ALREADY USED : {}, {:#x}", bank_id, idx);
+                    }
+
+                    self.msgs[bank_id][idx].text[lang_idx] = str.as_str().to_string();                    
+                }
+            }
+        } else  {
+            println!("NO MATCH : {}", line);
+        }
+    }
+
+    fn export_html(&self, filepath: &Path, ignore_tags : bool) {
+        
+        let mut exporter = HTMLExporter::new(filepath);
+        exporter.begin();
+        exporter.set_headers();
+
+        for bank in &self.msgs {
+            for msg in bank.iter().filter(|msg| !msg.is_empty()) {
+                exporter.add_row(msg, ignore_tags);
+            }
+        }
+        exporter.end();
+    }
+}
+
+fn process_file(lines : impl Iterator<Item=std::string::String>, lang_id : usize, bank_id : usize, parser : &mut BMGParser) {
+    let iter = lines.skip_while(|l|  !RE.is_match(l) );
+    
+    for l in iter {
+        parser.feed_line(&l, lang_id, bank_id);
+    }
+
+}
+
+fn process_language(lang_idx : usize, lang_id : &str, parser : &mut BMGParser) {
+    let str_path = &format!("./res/Msg{}", lang_id);
+    let folder_path = Path::new(&str_path);
+
+    for (bank_id,&basename) in FILENAMES.iter().enumerate() {
+        let filename = basename.to_owned() + ".txt";
+        println!("{} {}", lang_id, filename);
+        if let Ok(file) = File::open(folder_path.join(&filename)) {
+            let buf_reader = BufReader::new(file);
+            process_file(buf_reader.lines().flatten(),lang_idx, bank_id,  parser);
+        } else {
+            println!("Couldn't open file {}" , filename);
+        }
+    }
+}
+
+fn main() {
+
+    let mut parser : BMGParser = Default::default();
+    
+    for (lang_idx, lang) in LANGUAGES.iter().enumerate() {
+        process_language(lang_idx,lang, &mut parser);
+    }
+
+    //parser.print();
+
+    parser.export_html(Path::new("test.html"), false);
+
+    println!("{:?}", parser.msgs[0][0x7e9].text[0]);
+    parser.msgs[0][0x7e9].print_tags(0);
+}
