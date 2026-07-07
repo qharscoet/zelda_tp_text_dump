58969949a167dea189db8590a9b3b305b4b3486d
Quentin Harscoët <harscoet.quentin@gmail.com>
2026-07-08 01:47:59 +0200

    Handle MID1 + code clean

--- src/bmg_raw_parser.rs
+++ src/bmg_raw_parser.rs
@@ -2,7 +2,6 @@ use std::{
     cmp::min, fmt::{self, Display}, fs::File, io::{self, Read}, ops::Range, str::Utf8Error,
 };
 
-use encoding_rs::mem::decode_latin1;
 use thiserror::Error;
 
 fn get_u32(data: &[u8], idx: usize) -> u32 {
@@ -70,7 +69,10 @@ impl DAT1Data {
 }
 
 struct MID1Data {
-
+    count : u16,
+    _format : u16,
+    _unknown : u32,
+    ids : Vec<u32>
 }
 
 
@@ -90,7 +92,29 @@ struct BMGSection {
 
 struct BMGData {
     header: BMGHeader,
-    sections: Vec<BMGSection>,
+    sections: [Option<BMGSection>; BMGData::SECTION_COUNT],
+}
+
+impl BMGData {
+    const SECTION_COUNT : usize = 6;
+    const INF1 : usize = 0;
+    const DAT1 : usize = 1;
+    const MID1 : usize = 2;
+    const STR1 : usize = 3;
+    const FLW1 : usize = 4;
+    const FLI1 : usize = 5;
+
+    fn get_idx(type_str : &str) -> Option<usize> {
+        match type_str {
+            "INF1" => Some(BMGData::INF1),
+            "DAT1" => Some(BMGData::DAT1),
+            "MID1" => Some(BMGData::MID1),
+            "STR1" => Some(BMGData::STR1),
+            "FLW1" => Some(BMGData::FLW1),
+            "FLI1" => Some(BMGData::FLI1),
+            _ => None
+        }
+    }
 }
 
 struct BMGRawParser {
@@ -109,7 +133,7 @@ impl BMGRawParser {
                 sections_cnt: 0,
                 encoding: 0,
             },
-            sections: Vec::new(),
+            sections: [const { None } ;6],
         });
 
        BMGRawParser { data, data_parsed: parsed}
@@ -120,8 +144,16 @@ impl BMGRawParser {
         &self.data_parsed.header
     }
 
-    fn get_sections(&self) -> &Vec<BMGSection> {
+    fn get_sections(&self) -> &[Option<BMGSection>; BMGData::SECTION_COUNT] {
         &self.data_parsed.sections
+    }
+
+    fn get_section(&self, idx : usize) -> Option<&BMGSectionData> {
+        if let Some(section) = &self.data_parsed.sections[idx] {
+            Some(&section.data)
+        } else {
+            None
+        }
     }
 
     fn print(&self) {
@@ -131,14 +163,14 @@ impl BMGRawParser {
         println!("Sections count: {}", header.sections_cnt);
         println!("Encoding: {}", header.encoding);
 
-        for section in self.get_sections() {
+        for section in self.get_sections().iter().flatten() {
             println!("Section type: {}", section.section_type);
             println!("Section size: {}", section.size);
             println!("Section data range: {:?}", section.range);
 
             match &section.data {
                 BMGSectionData::INF1(INF1Data{count,entry_size, pad, entries})=> {
-                    println!("\t INF1 Section: count={}, entry_size={}, pad={}", count, entry_size, pad);
+                    println!("\t INF1 Section: count={}, entry_size={}, entry_len={}", count, entry_size, entries.len());
                     println!("\t Entry 0: {}", entries[0]);
                     println!("\t Entry 1: {}", entries[1]);
                     println!("\t Entry 2: {}", entries[2]);
@@ -148,6 +180,13 @@ impl BMGRawParser {
                     println!("\t First 10 bytes: {:X?}", &dat1data.data[0..10]);
                     println!("\t First message: {}", dat1data.get_text_at(1, header.encoding));
                 },
+                BMGSectionData::MID1(mid1data) => {
+                    println!("\t MID1 Section: count {}", mid1data.count);
+                    println!("\t Length {}", mid1data.count);
+                    println!("\t ID[0] : {}", mid1data.ids[0]);
+                    println!("\t ID[1] : {}", mid1data.ids[1]);
+                    println!("\t ID[2] : {}", mid1data.ids[2]);
+                }
                 _ => {}
             }
         }
@@ -181,20 +220,13 @@ impl BMGRawParser {
                 let pad = get_u32(&section_data, 0x04);
 
                 println!("INF1 Section: count={}, entry_size={}, pad={}", count, entry_size, pad);
-                let mut entries = Vec::new();
-                let mut entry_offset = 0x08;
 
-                for _ in 0..count {
-                    let entry_offset_value = get_u32(&section_data, entry_offset);
-                    let attributes = section_data[entry_offset + 4..entry_offset + entry_size as usize].to_vec();
+                let entries = section_data[0x08..].chunks_exact(entry_size as usize).map(|entry| {
+                    let entry_offset_value = get_u32(&entry, 0x00);
+                    let attributes = entry[0x04..].to_vec();
 
-                    entries.push(INF1Entry {
-                        offset: entry_offset_value,
-                        attributes,
-                    });
-
-                    entry_offset += entry_size as usize;
-                }
+                    INF1Entry{offset:entry_offset_value, attributes}
+                }).collect();
 
                 Ok(BMGSectionData::INF1(INF1Data {
                     count,
@@ -207,6 +239,16 @@ impl BMGRawParser {
                 println!("DAT1 Section: size={}", section_data.len());
                 println!("First 10 bytes: {:X?}", &data[offset..offset+10]);
                 Ok(BMGSectionData::DAT1(DAT1Data { data: section_data.to_vec()}))
+            },
+            "MID1" => {
+                let count = get_u16(&section_data, 0x00);
+                let format = get_u16(&section_data, 0x02);
+                let unknown  = get_u32(&section_data, 0x04);
+
+                let ids : Vec<_> = section_data[0x08..].chunks_exact(4).map(|v| get_u32(v, 0)).collect();
+
+
+                Ok(BMGSectionData::MID1(MID1Data { count, _format : format, _unknown: unknown, ids: ids}))
             }
             _ => Ok(BMGSectionData::FLW1), // Placeholder for other section types
 
@@ -217,7 +259,7 @@ impl BMGRawParser {
 
     fn parse_data(data: &[u8]) -> Result<BMGData, BMGParseError> {
         let header = BMGRawParser::parse_header(&data)?;
-        let mut sections = Vec::new();
+        let mut sections = [const {None}; BMGData::SECTION_COUNT];
         let mut offset = 0x20;
 
         for _ in 0..header.sections_cnt {
@@ -227,17 +269,33 @@ impl BMGRawParser {
             let range_start = offset + 8;
             let range_end = min(offset + section_size as usize, data.len()); //size includes the header
             let range = range_start..range_end as usize;
-
-            sections.push(BMGSection {
-                section_type : section_type,
-                size : section_size,
-                range : range,
-                data: BMGRawParser::parse_section(data, offset)?
-            });
+            
+            if let Some(idx) = BMGData::get_idx(&section_type) {
+                sections[idx] = Some(BMGSection {
+                    section_type : section_type,
+                    size : section_size,
+                    range : range,
+                    data: BMGRawParser::parse_section(data, offset)?
+                });
+            }
+        
             offset += section_size as usize;
         }
 
         Ok(BMGData { header, sections })
+    }
+
+
+    pub fn get_msg(&self, id : usize) -> String {
+        if let Some(BMGSectionData::INF1(inf1)) = self.get_section(BMGData::INF1) {
+            if let Some(BMGSectionData::DAT1(dat1)) = self.get_section(BMGData::DAT1) {
+                dat1.get_text_at(inf1.entries[id].offset as usize, self.data_parsed.header.encoding)
+            } else {
+                String::new()
+            }
+        } else {
+            String::new()
+        }
     }
 }
 
@@ -255,6 +313,8 @@ pub fn print_bmg(path : &str) {
     match open_bmg(path, 0) {
         Ok(parser) => {
             parser.print();
+
+            println!("Message 0x66 : {}", parser.get_msg(0x66));
         }
         Err(e) => {
             eprintln!("Error opening BMG file: {}", e);
